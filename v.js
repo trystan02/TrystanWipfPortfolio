63692d873a9301e08b68977ea32fdd5338699099
trystan02 <145148374+trystan02@users.noreply.github.com>
2024-06-13 12:43:43 -0600

    Update v.js

--- v.js
+++ v.js
@@ -1,18 +1,4 @@
-function downloadFile(url, fileName) {
-  fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-    .then(res => res.blob())
-    .then(res => {
-      const aElement = document.createElement('a');
-      aElement.setAttribute('download', fileName);
-      const href = URL.createObjectURL(res);
-      aElement.href = href;
-      aElement.setAttribute('target', '_blank');
-      aElement.click();
-      URL.revokeObjectURL(href);
-    });
-};
+
 function vi(){
-  downloadFile('ha.png'](ha.png'),
-'ha.png')
   alert("You Have Installed My Viruse");
 }
